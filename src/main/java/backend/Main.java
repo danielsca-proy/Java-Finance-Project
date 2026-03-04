863c039349102a3aa03c86628fd37f04f039cd51
Daniel <danielscalante4114@gmail.com>
2026-03-04 11:02:37 -0300

    Prueba de commits

--- src/main/java/backend/Main.java
+++ src/main/java/backend/Main.java
@@ -4,6 +4,6 @@ import backend.View.VistaLogin;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hola");
+        System.out.println("Hola Lean, se ve?");
     }
 }
